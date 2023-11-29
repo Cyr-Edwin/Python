bccf463070d2cd74fb5f48532481a472a8a2bf79
pyEdwin <cyredwin1@gmail.com>
2023-11-29 15:40:18 -0500

    add app.py file

---
+++ python_project_1/app.py
@@ -0,0 +1,31 @@
+# import modules
+from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
+
+# Get URL from the User
+url = input("Enter your URL here:")
+
+# Checked validity of the URL
+try:
+    # Example: url = "https://www.python.org/"
+    
+    resp = urlopen(url)
+    print("status code: " + str(resp.status))
+    print("Message: " + resp.reason)
+    print("Message: " + str(resp.headers))
+
+except HTTPError as err:
+    # Example: url = "http://httpbin.org/status/404"
+
+    # Display messages
+    print("status code for HTTPError : " + str(err.code))
+    print("Message for HTTPError: " + err.reason) 
+
+except URLError as e:
+    # Example: url= "https://www.python.o"
+
+    # Retrieve the first element of the List and replace "[" with an empty space
+    print("status code for URLError: " + str(e.reason).split(']')[0].replace('[',''))
+
+    # Retrieve the second element of the List
+    print("Message for URLError : " + str(e.reason).split(']')[1])
